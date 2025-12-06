97e3fa7f48d3e4c5b8336388c4865c1289a22451
Gustavo Bañares <gustavobbacco@gmail.com>
2025-12-05 22:41:41 -0300

    feat: enhance InputSingleFile component with file validation and update props in App

--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -1,18 +1,18 @@
-import { type VariantProps, tv } from 'tailwind-variants'
+import { tv, type VariantProps } from 'tailwind-variants'
 import Icon from './icon'
 import Text, { textVariants } from './text'
-import UpdateFileIcon from '../assets/icons/upload-file.svg?react'
+import UploadFileIcon from '../assets/icons/upload-file.svg?react'
 import FileImageIcon from '../assets/icons/image.svg?react'
-import React from 'react'
 import { useWatch } from 'react-hook-form'
+import React from 'react'
 
 export const inputSingleFileVariants = tv({
   base: `
     flex flex-col items-center justify-center w-full
     border border-solid border-border-primary
-    group-hover:border-border-active
+  group-hover:border-border-active
     rounded-lg gap-1 transition
-    `,
+  `,
   variants: {
     size: {
       md: 'px-5 py-6',
@@ -24,9 +24,7 @@ export const inputSingleFileVariants = tv({
 })
 
 export const inputSingleFileIconVariants = tv({
-  base: `
-   fill-placeholder
-    `,
+  base: 'fill-placeholder',
   variants: {
     size: {
       md: 'w-8 h-8',
@@ -41,25 +39,48 @@ interface InputSingleFileProps
   extends
     VariantProps<typeof inputSingleFileVariants>,
     Omit<React.ComponentProps<'input'>, 'size'> {
+  error?: React.ReactNode
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   form: any
-  error?: React.ReactNode
+  allowedExtensions: string[]
+  maxFileSizeInMB: number
 }
 
-export default function InputSingleFile({ size, error, form, ...props }: InputSingleFileProps) {
+export default function InputSingleFile({
+  size,
+  error,
+  form,
+  allowedExtensions,
+  maxFileSizeInMB,
+  ...props
+}: InputSingleFileProps) {
   const formValues = useWatch({ control: form.control })
   const name = props.name || ''
   const formFile: File = React.useMemo(() => formValues[name]?.[0], [formValues, name])
 
-  const handleRemoveFile = () => {
-    if (name && form.setValue) {
-      form.setValue(name, null)
-    }
+  const { fileExtension, fileSize } = React.useMemo(
+    () => ({
+      fileExtension: formFile?.name?.split('.')?.pop()?.toLowerCase() || '',
+      fileSize: formFile?.size || 0,
+    }),
+    [formFile]
+  )
+
+  function isValidExtension() {
+    return allowedExtensions.includes(fileExtension)
+  }
+
+  function isValidSize() {
+    return fileSize <= maxFileSizeInMB * 1024 * 1024
+  }
+
+  function isValidFile() {
+    return isValidExtension() && isValidSize()
   }
 
   return (
     <div>
-      {!formFile ? (
+      {!formFile || !isValidFile() ? (
         <>
           <div className="w-full relative group cursor-pointer">
             <input
@@ -68,7 +89,7 @@ export default function InputSingleFile({ size, error, form, ...props }: InputSi
               {...props}
             />
             <div className={inputSingleFileVariants({ size })}>
-              <Icon svg={UpdateFileIcon} className={inputSingleFileIconVariants({ size })} />
+              <Icon svg={UploadFileIcon} className={inputSingleFileIconVariants({ size })} />
               <Text variant="label-medium" className="text-placeholder text-center">
                 Arraste o arquivo aqui
                 <br />
@@ -76,11 +97,23 @@ export default function InputSingleFile({ size, error, form, ...props }: InputSi
               </Text>
             </div>
           </div>
-          {error && (
-            <Text variant="label-small" className="text-accent-red">
-              {error}
-            </Text>
-          )}
+          <div className="flex flex-col gap-1 mt-1">
+            {formFile && !isValidExtension() && (
+              <Text variant="label-small" className="text-accent-red">
+                Tipo de arquivo inválido
+              </Text>
+            )}
+            {formFile && !isValidSize() && (
+              <Text variant="label-small" className="text-accent-red">
+                Tamanho do arquivo ultrapassa o máximo
+              </Text>
+            )}
+            {error && (
+              <Text variant="label-small" className="text-accent-red">
+                Erro no campo
+              </Text>
+            )}
+          </div>
         </>
       ) : (
         <div className="flex gap-3 items-center border border-solid border-border-primary mt-5 p-3 rounded">
@@ -91,18 +124,20 @@ export default function InputSingleFile({ size, error, form, ...props }: InputSi
                 {formFile.name}
               </Text>
             </div>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={handleRemoveFile}
-              className={textVariants({
-                variant: 'label-small',
-                className: 'text-accent-red cursor-pointer hover:underline',
-              })}
-            >
-              Remover
-            </button>
+            <div className="flex">
+              <button
+                type="button"
+                className={textVariants({
+                  variant: 'label-small',
+                  className: 'text-accent-red cursor-pointer hover:underline',
+                })}
+                onClick={() => {
+                  form.setValue(name, undefined)
+                }}
+              >
+                Remover
+              </button>
+            </div>
           </div>
         </div>
       )}
